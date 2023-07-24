0de74e0560c69ae0960ee603ae606debe984ae41
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-24 18:31:46 +0200

    [init] - initial commit

---
+++ src/main/java/com/sda/rentalloapp/RentalloAppApplication.java
@@ -0,0 +1,13 @@
+package com.sda.rentalloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalloAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalloAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/rentalloapp/RentalloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.rentalloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
