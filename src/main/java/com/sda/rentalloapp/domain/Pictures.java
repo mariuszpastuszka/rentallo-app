1ceb704f8c69a9141dfa29f762ad16f153f307bb
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-24 21:04:40 +0200

    [change] - Car entity almost finished :)

--- src/main/java/com/sda/rentalloapp/domain/Pictures.java
+++ src/main/java/com/sda/rentalloapp/domain/Pictures.java
@@ -1,11 +1,15 @@
 package com.sda.rentalloapp.domain;
 
+import jakarta.persistence.Embeddable;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Embeddable
 public class Pictures {
+
     String mainPictureUrl;
+
     List<String> picturesUrls;
 }
