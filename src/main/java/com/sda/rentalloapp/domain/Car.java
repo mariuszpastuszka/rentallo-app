392aa8801a365f2943e08bd9bab1a09567b6fcd1
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-24 20:59:37 +0200

    [add] - Car's enums property implemented

--- src/main/java/com/sda/rentalloapp/domain/Car.java
+++ src/main/java/com/sda/rentalloapp/domain/Car.java
@@ -3,21 +3,49 @@ package com.sda.rentalloapp.domain;
 import com.sda.rentalloapp.domain.enumeration.BodyType;
 import com.sda.rentalloapp.domain.enumeration.EngineType;
 import com.sda.rentalloapp.domain.enumeration.FuelType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "CARS")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
     String model;
+
     String brand;
+
+    @Enumerated(EnumType.STRING)
     FuelType fuelType;
+
+    @Enumerated(EnumType.STRING)
     EngineType engineType;
+
+    @Enumerated(EnumType.STRING)
     BodyType bodyType;
+
     int numberOfSeats;
+
     int trunkCapacityInLitres;
+
     String combustionPer100Km;
+
     String bodySerialNumber;
+
     int pricePerDayInPolishGrosz;
+
     boolean available;
+
     int rangeInKm;
+
     Pictures pictures;
 }
--- src/main/java/com/sda/rentalloapp/domain/enumeration/FuelType.java
+++ src/main/java/com/sda/rentalloapp/domain/enumeration/FuelType.java
@@ -1,5 +1,19 @@
 package com.sda.rentalloapp.domain.enumeration;
 
 public enum FuelType {
-    // TODO
+    GAS("gas fuel"),
+    OIL("oil fuel"),
+    GASOLINE("gasoline fuel"),
+    ELECTRIC("electricity"),
+    HYBRID("hybrid");
+
+    FuelType(String fuelDesc) {
+        this.fuelDesc = fuelDesc;
+    }
+
+    public String getFuelDesc() {
+        return fuelDesc;
+    }
+
+    private String fuelDesc;
 }
