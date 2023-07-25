279c45a96ddf470cb2457b29f0e2fabd96788cd4
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-25 20:35:53 +0200

    [add] - CarDto first implementation

---
+++ src/main/java/com/sda/rentalloapp/dto/CarDto.java
@@ -0,0 +1,22 @@
+package com.sda.rentalloapp.dto;
+
+import com.sda.rentalloapp.domain.enumeration.BodyType;
+import com.sda.rentalloapp.domain.enumeration.EngineType;
+import com.sda.rentalloapp.domain.enumeration.FuelType;
+
+public record CarDto(
+        Long id,
+        String model,
+        String brand,
+        FuelType fuelType,
+        EngineType engineType,
+        BodyType bodyType,
+        int numberOfSeats,
+        int trunkCapacityInLitres,
+        String combustionPer100Km,
+        String bodySerialNumber,
+        int pricePerDayInPolishGrosz,
+        boolean available,
+        int rangeInKm
+) {
+}
