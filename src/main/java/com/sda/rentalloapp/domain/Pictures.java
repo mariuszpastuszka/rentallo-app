a858b30348c6b472f4d242df3b46e8216a74224c
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-25 18:40:17 +0200

    [change] - embeddable Pictures implementation improved

--- src/main/java/com/sda/rentalloapp/domain/Pictures.java
+++ src/main/java/com/sda/rentalloapp/domain/Pictures.java
@@ -1,5 +1,6 @@
 package com.sda.rentalloapp.domain;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
 import lombok.Data;
 
@@ -11,5 +12,6 @@ public class Pictures {
 
     String mainPictureUrl;
 
+    @ElementCollection
     List<String> picturesUrls;
 }
