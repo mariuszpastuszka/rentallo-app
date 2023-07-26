086ec6d834d5740dffe4ee726cee7a67887c8922
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-26 18:07:11 +0200

    [change] - CarMapper - fromEntityToDto implemented

--- src/main/java/com/sda/rentalloapp/dto/CarDto.java
+++ src/main/java/com/sda/rentalloapp/dto/CarDto.java
@@ -3,7 +3,9 @@ package com.sda.rentalloapp.dto;
 import com.sda.rentalloapp.domain.enumeration.BodyType;
 import com.sda.rentalloapp.domain.enumeration.EngineType;
 import com.sda.rentalloapp.domain.enumeration.FuelType;
+import lombok.Builder;
 
+@Builder
 public record CarDto(
         Long id,
         String model,
--- src/main/java/com/sda/rentalloapp/mapper/CarMapper.java
+++ src/main/java/com/sda/rentalloapp/mapper/CarMapper.java
@@ -2,13 +2,29 @@ package com.sda.rentalloapp.mapper;
 
 import com.sda.rentalloapp.domain.Car;
 import com.sda.rentalloapp.dto.CarDto;
+import com.sda.rentalloapp.dto.PicturesDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CarMapper implements Mapper<Car, CarDto> {
     @Override
     public CarDto fromEntityToDto(Car entity) {
-        return null;
+        return CarDto.builder()
+                .id(entity.getId())
+                .model(entity.getModel())
+                .brand(entity.getBrand())
+                .fuelType(entity.getFuelType())
+                .engineType(entity.getEngineType())
+                .bodyType(entity.getBodyType())
+                .numberOfSeats(entity.getNumberOfSeats())
+                .trunkCapacityInLitres(entity.getTrunkCapacityInLitres())
+                .combustionPer100Km(entity.getCombustionPer100Km())
+                .bodySerialNumber(entity.getBodySerialNumber())
+                .pricePerDayInPolishGrosz(entity.getPricePerDayInPolishGrosz())
+                .available(entity.isAvailable())
+                .rangeInKm(entity.getRangeInKm())
+                .pictures(new PicturesDto(entity.getPictures().getMainPictureUrl(), entity.getPictures().getPicturesUrls()))
+                .build();
     }
 
     @Override
