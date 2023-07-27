aab745b1ae3009172318e9c2ce6d31cf8629b336
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-07-27 18:46:30 +0200

    [add] - endpoint for new cars - in progress

--- src/main/java/com/sda/rentalloapp/controller/CarRestController.java
+++ src/main/java/com/sda/rentalloapp/controller/CarRestController.java
@@ -3,11 +3,9 @@ package com.sda.rentalloapp.controller;
 import com.sda.rentalloapp.dto.CarDto;
 import com.sda.rentalloapp.mapper.CarMapper;
 import com.sda.rentalloapp.service.CarService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,5 +36,10 @@ public class CarRestController {
     public CarDto getCarById(@PathVariable("id") Long carId) {
         log.info("trying to find car with id: [{}]", carId);
         return carMapper.fromEntityToDto(carService.findCarById(carId));
+    }
+
+    @PostMapping("/cars")
+    public void addCar(@RequestBody @Valid CarDto toSave) {
+        log.info("adding new car: [{}]", toSave);
     }
 }
--- src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
+++ src/main/java/com/sda/rentalloapp/controller/CarRestControllerAdvisor.java
@@ -4,6 +4,7 @@ import com.sda.rentalloapp.dto.ResponseDto;
 import com.sda.rentalloapp.exception.WrongCarIdException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,9 +19,7 @@ public class CarRestControllerAdvisor {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(WrongCarIdException.class)
     public ResponseDto handleCarNotFoundException(WrongCarIdException exc) {
-        String path = ServletUriComponentsBuilder.fromCurrentRequest()
-                .build()
-                .getPath();
+        String path = getCurrentRequestPath();
 
 
         return ResponseDto.builder()
@@ -31,4 +30,25 @@ public class CarRestControllerAdvisor {
                 .path(path)
                 .build();
     }
+
+    private static String getCurrentRequestPath() {
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                .build()
+                .getPath();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseDto handleBadRequest(MethodArgumentNotValidException exc) {
+        String path = getCurrentRequestPath();
+
+        return ResponseDto.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .path(path)
+                .message(exc.getMessage())
+                .build();
+    }
+
 }
