59d3b72934e6fac7c600f8faa0cea3c6a9a0aea7
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-07 20:54:14 +0200

    [add] - ClientDto created

--- backend-service/src/main/java/com/sda/rentalloapp/dto/ClientDto.java
+++ backend-service/src/main/java/com/sda/rentalloapp/dto/ClientDto.java
@@ -1,5 +1,17 @@
 package com.sda.rentalloapp.dto;
 
-// TODO
-public record ClientDto() {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record ClientDto(
+        Long id,
+        String name,
+        String surname,
+        String pesel,
+        String email,
+        LocalDate dateOfBirth,
+        String phone,
+        AddressDto addressDto,
+        LocalDateTime accountCreated
+) {
 }
