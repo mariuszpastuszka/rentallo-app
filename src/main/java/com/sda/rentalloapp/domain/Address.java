d4d3c3ca6174a0bfd215d21f7b6a1ea47b317349
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-07 19:02:58 +0200

    [change] - translation of domain objects into entities - in progress

--- src/main/java/com/sda/rentalloapp/domain/Address.java
+++ src/main/java/com/sda/rentalloapp/domain/Address.java
@@ -1,7 +1,22 @@
 package com.sda.rentalloapp.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ADDRESSES")
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String street;
     String zipCode;
     String city;
--- src/main/java/com/sda/rentalloapp/domain/Client.java
+++ src/main/java/com/sda/rentalloapp/domain/Client.java
@@ -1,19 +1,32 @@
 package com.sda.rentalloapp.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CLIENTS")
 public class Client {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     String name;
     String surname;
     String pesel;
     String email;
     LocalDate dateOfBirth;
     String phone;
+    @ManyToOne
     Address address;
     LocalDateTime accountCreated;
 }
