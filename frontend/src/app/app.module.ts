956741835e26853daa343d1103443ceefd9e4250
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-08 18:39:40 +0200

    [add] - material design applied

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './components/app/app.component';
 import { CarsComponent } from './components/cars/cars.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CarsComponent } from './components/cars/cars.component';
     CarsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
