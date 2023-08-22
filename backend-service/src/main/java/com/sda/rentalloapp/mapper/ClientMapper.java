f06163f7989abfa2a774804e3aa8b69b4e19be8a
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-22 20:45:32 +0200

    [change] - ClientMapper implemented

--- backend-service/src/main/java/com/sda/rentalloapp/mapper/ClientMapper.java
+++ backend-service/src/main/java/com/sda/rentalloapp/mapper/ClientMapper.java
@@ -6,13 +6,40 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClientMapper implements Mapper<Client, ClientDto> {
+
+    private final AddressMapper addressMapper;
+
+    public ClientMapper(AddressMapper addressMapper) {
+        this.addressMapper = addressMapper;
+    }
+
     @Override
     public ClientDto fromEntityToDto(Client entity) {
-        return null;
+        return ClientDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .surname(entity.getSurname())
+                .pesel(entity.getPesel())
+                .email(entity.getEmail())
+                .dateOfBirth(entity.getDateOfBirth())
+                .phone(entity.getPhone())
+                .addressDto(addressMapper.fromEntityToDto(entity.getAddress()))
+                .accountCreated(entity.getAccountCreated())
+                .build();
     }
 
     @Override
     public Client fromDtoToEntity(ClientDto dto) {
-        return null;
+        return Client.builder()
+                .id(dto.id())
+                .name(dto.name())
+                .surname(dto.surname())
+                .pesel(dto.pesel())
+                .email(dto.email())
+                .dateOfBirth(dto.dateOfBirth())
+                .phone(dto.phone())
+                .address(addressMapper.fromDtoToEntity(dto.addressDto()))
+                .accountCreated(dto.accountCreated())
+                .build();
     }
 }
