1cd41fb0d7b078c10b03ee0728ef2cf6deaf284d
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-22 18:03:43 +0200

    [add] - top bar component created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { CarsComponent } from './components/cars/cars.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent
+    CarsComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
