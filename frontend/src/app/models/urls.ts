79dee17505e659be6186172a4d847d6a03d43de7
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-22 18:46:21 +0200

    [change] - application links defined

--- frontend/src/app/models/urls.ts
+++ frontend/src/app/models/urls.ts
@@ -1,2 +1,11 @@
+// external links
 const serverUrl = 'http://localhost:8080'
 export const allCarsUrl = `${serverUrl}/api/cars`
+
+// internal links
+export const homePageUrl = ''
+export const rentsHistoryUrl = 'rents-history'
+export const carsUrl = 'cars'
+export const clientsUrl = 'clients'
+export const aboutUrl = 'about'
+export const notFoundPageUrl = '**'
