059626d0a6e2f36528058f9855d44a5d4bf32029
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-22 20:58:35 +0200

    [change] - clients test data

--- backend-service/src/main/java/com/sda/rentalloapp/config/DataInitializer.java
+++ backend-service/src/main/java/com/sda/rentalloapp/config/DataInitializer.java
@@ -1,17 +1,22 @@
 package com.sda.rentalloapp.config;
 
 
+import com.sda.rentalloapp.domain.Address;
 import com.sda.rentalloapp.domain.Car;
+import com.sda.rentalloapp.domain.Client;
 import com.sda.rentalloapp.domain.Pictures;
 import com.sda.rentalloapp.domain.enumeration.BodyType;
 import com.sda.rentalloapp.domain.enumeration.EngineType;
 import com.sda.rentalloapp.domain.enumeration.FuelType;
+import com.sda.rentalloapp.repository.AddressRepository;
 import com.sda.rentalloapp.repository.CarRepository;
+import com.sda.rentalloapp.repository.ClientRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Component
@@ -21,12 +26,60 @@ public class DataInitializer implements CommandLineRunner {
 
     private final CarRepository carRepository;
 
-    public DataInitializer(CarRepository carRepository) {
+    private final AddressRepository addressRepository;
+
+    private final ClientRepository clientRepository;
+
+    public DataInitializer(CarRepository carRepository, AddressRepository addressRepository, ClientRepository clientRepository) {
         this.carRepository = carRepository;
+        this.addressRepository = addressRepository;
+        this.clientRepository = clientRepository;
     }
 
     @Override
     public void run(String... args) {
+        createCars();
+        createClientsWithAddresses();
+    }
+
+    private void createClientsWithAddresses() {
+        log.info("let's create some locations");
+
+        Address bieszczady = Address.builder()
+                .street("Bieszczadzka")
+                .zipCode("01-123")
+                .houseNumber("11")
+                .voivodeship("Podkarpackie")
+                .country("Poland")
+                .build();
+        addressRepository.save(bieszczady);
+
+        log.info("let's create some clients");
+
+        Client first = Client.builder()
+                .name("Jan")
+                .surname("K.")
+                .pesel("80103014444")
+                .email("john.k@gmail.com")
+                .dateOfBirth(LocalDate.of(1980, 10, 30))
+                .phone("111-111-777")
+                .address(bieszczady)
+                .build();
+        Client second = Client.builder()
+                .name("Helena")
+                .surname("K.")
+                .pesel("81103014444")
+                .email("helena.k@gmail.com")
+                .dateOfBirth(LocalDate.of(1981, 10, 30))
+                .phone("111-111-775")
+                .address(bieszczady)
+                .build();;
+
+        clientRepository.save(first);
+        clientRepository.save(second);
+    }
+
+    private void createCars() {
         log.info("let's create some cars");
         Car mazda = Car.builder()
                 .model("V")
