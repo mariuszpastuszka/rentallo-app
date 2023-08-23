ea7b986815dd0f4c6e97d887d2b2ca29fe1a63e5
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-23 18:43:13 +0200

    [change] - fetching clients from backend - done

--- frontend/src/app/components/clients/clients.component.ts
+++ frontend/src/app/components/clients/clients.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from "../../services/client/client.service";
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
 
+  constructor(
+    private clientService: ClientService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.clientService.getClients()
+      .subscribe(clients => {
+        console.log(`Clients from server: ${JSON.stringify(clients, null, 2)}`)
+      })
+  }
 }
