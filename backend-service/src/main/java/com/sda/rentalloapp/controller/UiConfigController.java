c6f7240e7392e47ff19f135baf765ce366294840
Mariusz Pastuszka <mpastuszka@gmail.com>
2023-08-28 20:07:11 +0200

    [change] - ui config controller - body types implemented

--- backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
+++ backend-service/src/main/java/com/sda/rentalloapp/controller/UiConfigController.java
@@ -1,5 +1,6 @@
 package com.sda.rentalloapp.controller;
 
+import com.sda.rentalloapp.domain.enumeration.BodyType;
 import com.sda.rentalloapp.domain.enumeration.FuelType;
 import com.sda.rentalloapp.dto.ConfigItem;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,15 @@ public class UiConfigController {
          return Arrays.stream(FuelType.values())
                 .map(fuelType ->
                         new ConfigItem<>(fuelType.name(), fuelType.fuelDescription()))
+                .toList();
+    }
+
+    @GetMapping("/body-types")
+    public List<ConfigItem<String, String>> getCarBodyTypes() {
+        return Arrays.stream(BodyType.values())
+                .map(bodyType ->
+                        new ConfigItem<>(bodyType.name(),
+                                bodyType.description()))
                 .toList();
     }
 }
--- backend-service/src/main/java/com/sda/rentalloapp/domain/enumeration/BodyType.java
+++ backend-service/src/main/java/com/sda/rentalloapp/domain/enumeration/BodyType.java
@@ -1,10 +1,20 @@
 package com.sda.rentalloapp.domain.enumeration;
 
 public enum BodyType {
-    HATCHBACK,
-    SEDAN,
-    COMBI,
-    SUV,
-    PICKUP,
-    VAN
+    HATCHBACK("Hatchback"),
+    SEDAN("Sedan"),
+    COMBI("Combi"),
+    SUV("SUV"),
+    PICKUP("Pickup"),
+    VAN("VAN");
+
+    private final String description;
+
+    BodyType(String description) {
+        this.description = description;
+    }
+
+    public String description() {
+        return description;
+    }
 }
